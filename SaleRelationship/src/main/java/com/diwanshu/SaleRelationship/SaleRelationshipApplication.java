5857555e7fc63e335a3801b0956ce16b25d88f04
Diwanshu Tiwari <diwanshutiwar94@gmail.com>
2018-06-05 10:12:26 +0530

    Initaial Structure of Project

---
+++ SaleRelationship/src/main/java/com/diwanshu/SaleRelationship/SaleRelationshipApplication.java
@@ -0,0 +1,12 @@
+package com.diwanshu.SaleRelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleRelationshipApplication.class, args);
+	}
+}
